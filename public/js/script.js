c590e9b333e1758b6ddebe28b7969fc319b9af1c
Zandolor <steven.j.jordaan2000@gmail.com>
2021-05-13 22:38:50 +0200

    Add check for api status

--- public/js/script.js
+++ public/js/script.js
@@ -2,13 +2,14 @@ const API_URL = 'https://game-engine-api.herokuapp.com/'
 
 const getApiRoot = () => {
 
+  let element = document.getElementById('api-status');
+
   fetch(API_URL).then(res => {
     return res.json();
   }).then(res => {
-    let main = document.getElementsByTagName('main');
-    main[0].innerHTML(res);
+    element.innerHTML = res;
+  }).catch(error => {
+    element.innerHTML = 'API could not be reached';
   });
 
 }
-
-getApiRoot();

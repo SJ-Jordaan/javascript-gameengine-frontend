e4dee7ece0f318600b38c6f3543a62c501455ee4
Solly <solomonm@bbd.co.za>
2021-05-20 11:02:48 +0200

    Added the solly folder

---
+++ solly/javascript/googleInitializer.js
@@ -0,0 +1,14 @@
+//import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyDAPvdG6qJJ5oIB_s4KZ8cutcO4u0y5PIE",
+    authDomain: "game-engine-313611.firebaseapp.com",
+    projectId: "game-engine-313611",
+    storageBucket: "game-engine-313611.appspot.com",
+    messagingSenderId: "223785516742",
+    appId: "1:223785516742:web:aab62343f5d5c6bf3661c7",
+    measurementId: "G-0FCE2LCJ98"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // module.exports = firebase;
---
+++ solly/javascript/sign-up.js
@@ -0,0 +1,73 @@
+let message = [];
+
+function signUp(){
+    document.getElementById("signUpForm").addEventListener('submit', function(event){event.preventDefault();});
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+    
+    if(validateFields(name,email,password1,password2)){
+        firebase.auth().createUserWithEmailAndPassword(email, password1)
+    .then(async (userCredential) => {
+    // Signed in 
+        var user = userCredential.user;
+        const toast = document.getElementById("snackbar");
+        toast.className = "show";
+        toast.innerHTML = `successfully create user: ${userCredential.email}`
+        console.log(userCredential);
+        await setTimeout(() => {
+            toast
+            toast.className = toast.className.replace("show", "");
+            //window.location = '/index.html'
+        }, 4000);
+        //firebase.auth().signOut();
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        const toast = document.getElementById("snackbar");
+        toast.className = "show";
+        toast.innerHTML = `${errorMessage}`
+    // ..
+    });
+    
+    }
+    const toast = document.getElementById("snackbar");
+    toast.className = "show";
+    toast.innerHTML = `${message}`
+    setTimeout(() => {
+        toast.className = toast.className.replace("show", "");
+    }, 6000);
+   
+}
+function validateFields(name,email,password1,password2){
+    message=[];
+    let isValidated = true;
+    if(name.length === 0){
+        message.push(" Enter name ")
+        isValidated = false;
+    }
+    if(email.length === 0){
+    message.push(" Enter email address ");
+    isValidated = false;
+    }
+    if(password1.length === 0 || password2.length === 0){
+      message.push(" Enter Password ")
+      isValidated = false;
+    }
+    if(password1.length <  6){
+        message.push(" Password length must be greater than 6")
+        isValidated = false;
+      }
+    if(!(password1 === password2)){
+      message.push(" Passwords must match ");
+      return false;
+    }
+
+    
+
+    return isValidated;
+  }

1d07b3388011c646e15518571f8d4ba24f6501b7
SJ-Jordaan <u18074848@tuks.co.za>
2021-05-21 09:26:56 +0200

    Fix routing issues

--- public/scripts/signin.js
+++ public/scripts/signin.js
@@ -1,101 +1,100 @@
 let message = [];
 firebase.auth().onAuthStateChanged(function (user) {
-	if (user) {
-		const user = firebase.auth().currentUser;
+  if (user) {
+    const user = firebase.auth().currentUser;
 
-		if (user != null) {
-			const email_id = user.uid;
-			console.log(`assigned in is: ${email_id}`);
-		}
-	} else {
-		const toast = document.getElementById("snackbar");
-		toast.className = "show";
-		toast.innerHTML = "Sign in.";
-		setTimeout(() => {
-			toast;
-			toast.className = toast.className.replace("show", "");
-		}, 4000);
-	}
+    if (user != null) {
+      const email_id = user.uid;
+      console.log(`assigned in is: ${email_id}`);
+    }
+  } else {
+    const toast = document.getElementById("snackbar");
+    toast.className = "show";
+    toast.innerHTML = "Sign in.";
+    setTimeout(() => {
+      toast;
+      toast.className = toast.className.replace("show", "");
+    }, 4000);
+  }
 });
 
 function logIn() {
-	document
-		.getElementById("loginForm")
-		.addEventListener("submit", function (event) {
-			event.preventDefault();
-		});
+  document
+    .getElementById("loginForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+    });
 
-	const userEmail = document.getElementById("username").value;
-	const userPassword = document.getElementById("password").value;
+  const userEmail = document.getElementById("username").value;
+  const userPassword = document.getElementById("password").value;
 
-	if (validateFields(userPassword, userEmail)) {
-		firebase
-			.auth()
-			.signInWithEmailAndPassword(userEmail, userPassword)
-			.then((userCredential) => {
-				// Signed in
+  if (validateFields(userPassword, userEmail)) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(userEmail, userPassword)
+      .then((userCredential) => {
+        // Signed in
 
-				fetch("https://game-engine-api.herokuapp.com/api/auth/signin", {
-					method: "POST",
-					body: JSON.stringify({
-						uid: userCredential.uid,
-					}),
-					headers: {
-						"Content-type": "application/json; charset=UTF-8",
-					},
-				})
-					.then((resp) => resp.json())
-					.then((result) => {
-						console.log(result);
-						sessionStorage.setItem("user", JSON.stringify(result));
-						window.location = "/explore";
+        fetch("https://game-engine-api.herokuapp.com/api/auth/signin", {
+          method: "POST",
+          body: JSON.stringify({
+            uid: userCredential.uid,
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        })
+          .then((resp) => resp.json())
+          .then((result) => {
+            console.log(result);
+            sessionStorage.setItem("user", JSON.stringify(result));
+            window.location = "/create/terms-of-use";
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        const user = userCredential.user;
 
-					})
-					.catch((error) => {
-						console.error(error);
-					});
-				const user = userCredential.user;
+        console.log(`Successfully signed in user details: ${user}`);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
 
-				console.log(`Successfully signed in user details: ${user}`);
-				// ...
-			})
-			.catch((error) => {
-				const errorCode = error.code;
-				const errorMessage = error.message;
+        console.log(`Error: ${errorMessage} status: ${errorCode}`);
 
-				console.log(`Error: ${errorMessage} status: ${errorCode}`);
-
-				const toast = document.getElementById("snackbar");
-				toast.className = "show";
-				toast.innerHTML = `${errorMessage}`;
-				setTimeout(() => {
-					toast.className = toast.className.replace("show", "");
-				}, 5000);
-			});
-	}
-	const toast = document.getElementById("snackbar");
-	toast.className = "show";
-	toast.innerHTML = `${message}`;
-	setTimeout(() => {
-		toast.className = toast.className.replace("show", "");
-	}, 5000);
+        const toast = document.getElementById("snackbar");
+        toast.className = "show";
+        toast.innerHTML = `${errorMessage}`;
+        setTimeout(() => {
+          toast.className = toast.className.replace("show", "");
+        }, 5000);
+      });
+  }
+  const toast = document.getElementById("snackbar");
+  toast.className = "show";
+  toast.innerHTML = `${message}`;
+  setTimeout(() => {
+    toast.className = toast.className.replace("show", "");
+  }, 5000);
 }
 function validateFields(password, email) {
-	message = [];
-	let isValidated = true;
-	if (password.length === 0) {
-		message.push("Enter Password");
-		isValidated = false;
-	}
-	if (password.length < 6) {
-		message.push(" Password length must be greater than 6 ");
-		isValidated = false;
-	}
+  message = [];
+  let isValidated = true;
+  if (password.length === 0) {
+    message.push("Enter Password");
+    isValidated = false;
+  }
+  if (password.length < 6) {
+    message.push(" Password length must be greater than 6 ");
+    isValidated = false;
+  }
 
-	if (email.length === 0) {
-		message.push(" Enter email address ");
-		isValidated = false;
-	}
+  if (email.length === 0) {
+    message.push(" Enter email address ");
+    isValidated = false;
+  }
 
-	return isValidated;
+  return isValidated;
 }
